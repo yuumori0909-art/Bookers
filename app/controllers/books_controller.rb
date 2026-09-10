29ed9f8c1e85b4c9ff6c9354e12d24141ba562da
Ubuntu <ubuntu@ip-172-31-18-99.ap-northeast-1.compute.internal>
2026-09-10 05:23:43 +0000

    [add] indexとshowの編集完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,9 +12,11 @@ class BooksController < ApplicationController
   end
 
   def index
+    @books = Book.all 
   end
 
   def show
+    @book = Book.find(params[:id]) 
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   get "books/new"
   post 'books' => 'books#create'  
-  get "books/index"
-  get "books/show"
+
+  get 'books' => 'books#index'
+   get 'books/:id' => 'books#show', as: 'book'
+
   get "books/edit"
+  
   get '/top' => 'homes#top'
   root "homes#top"
 
